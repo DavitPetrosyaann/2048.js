af163131352d99bcc1255bc995515381d3e33440
DeimeralGIT <vahanhekimyan99@gmail.com>
2026-07-04 22:23:56 +0400

    final changes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,6 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
       base: '/games/2048/',
-      define: {
-        // This is just generic value for the GEMINI API key.
-        // This is not used at all, and can be ignored!
-        'process.env.API_KEY' : JSON.stringify('api-key-this-is-not-used-can-be-ignored!'),
-      },
-      server: {
-        proxy: {
-          //Target your Node.js backend
-          '/api-proxy': 'http://localhost:5000',
-          '/ws-proxy': {target: 'ws://localhost:5000', ws: true},
-        },
-      },
       plugins: react(),
       resolve: {
         alias: {
